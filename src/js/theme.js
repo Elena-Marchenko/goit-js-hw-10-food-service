7956d3e319b74095e9b250ab4298b68c1590ce7a
Elena-Marchenko <Leawest91@gmail.com>
2021-04-30 15:11:45 +0300

    Делает три правки

--- src/js/theme.js
+++ src/js/theme.js
@@ -2,41 +2,53 @@ const Theme = {
   LIGHT: 'light-theme',
   DARK: 'dark-theme',
 };
+
 const THEME_KEY = 'theme';
 const TOGGLE_KEY = 'toggle';
 
 const refs = {
     toggle: document.querySelector('#theme-switch-toggle'),
     body: document.querySelector('body'),
-}
+};
 
-const storageTheme = window.localStorage.getItem(THEME_KEY);
-const storageToggle = window.localStorage.getItem(TOGGLE_KEY);
+const storageTheme = localStorage.getItem(THEME_KEY);
 
-refs.body.classList.add(storageTheme);
-refs.toggle.checked = storageToggle;
 
 refs.toggle.addEventListener('change', changeThemeInGallery);
 
+onStorageThemeDark();
+onStorageThemeLight();
+
 function changeThemeInGallery(e) {
-    
-        
     if (e.target.checked) {
         refs.body.classList.add(Theme.DARK);
+        localStorage.setItem(THEME_KEY, Theme.DARK);
         refs.body.classList.remove(Theme.LIGHT);
-        window.localStorage.setItem(THEME_KEY, Theme.DARK);
-        window.localStorage.setItem(TOGGLE_KEY, 'true');
-       
+
     } else {
         refs.body.classList.add(Theme.LIGHT);
         refs.body.classList.remove(Theme.DARK);
-        window.localStorage.setItem(THEME_KEY, Theme.LIGHT);
-        window.localStorage.removeItem(TOGGLE_KEY);
+        localStorage.setItem(THEME_KEY, Theme.LIGHT);
+        
     }
 };
 
-    
-    
+function onStorageThemeDark() {
+    if (storageTheme === Theme.DARK) {
+        refs.toggle.checked = storageTheme;
+    } 
+};
+
+function onStorageThemeLight() {
+    if (!storageTheme) {
+        refs.body.classList.add(Theme.LIGHT);
+        
+    } else {
+        refs.body.classList.add(storageTheme);
+    } 
+};
+ 
+
     
 
 
